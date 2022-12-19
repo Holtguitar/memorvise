e25cc1a8b32a9d15b5e07dbccf4c3bf9d0e5b1bb
Brandon <holtguitar@gmail.com>
2022-12-19 16:30:08 -0500

    delete redirect

--- src/main.js
+++ src/main.js
@@ -30,11 +30,11 @@
 //     .catch((error) => {
 //         alert(error.message)
 //     })
-import { createApp } from "vue";
+import { createApp, onRenderTriggered } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
 import "./assets/main.css";
 
 createApp(App).use(store).use(router).mount("#app");
+
